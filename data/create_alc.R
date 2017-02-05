256c3f2d7612483e71655f0b3f7b9e0bc86ad6fd
AnnikaLehmusSun <annika.lehmus@helsinki.fi>
2017-02-05 19:31:36 +0200

    chapter3
    assignment update

---
+++ data/create_alc.R
@@ -0,0 +1,7 @@
+Annika Lehmus-Sun
+February 5, 2017
+Creating alc data from two datasets
+
+Using Data Mining To Predict Secondary School Student Alcohol Consumption. 
+Fabio Pagnotta, Hossain Mohammad Amran 
+Department of Computer Science,University of Camerino
