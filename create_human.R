4e69e967fcecf4201fe52b301b4c00b091ac98a0
AnnikaLehmusSun <annika.lehmus@helsinki.fi>
2017-02-14 17:09:27 +0200

    chapter 4
    chapter 4 update vol 2

--- create_human.R
+++ create_human.R
@@ -1,9 +1,12 @@
 hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
 gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
-               
+
 str(hd)
 dim(hd)
 summary(hd)
+
+
+
 hd$HDI.Rank <- hd$RankHDI
 hd$Human.Development.Index..HDI. <- hd$HDI
 hd$Life.Expectancy.at.Birth <- hd$LifeExp
